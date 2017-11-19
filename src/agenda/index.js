c323c5f63178a0c8b344e99c7e13cc5b21c074e6
bjku <bjku@ucsd.edu>
2017-11-19 14:42:17 -0800

    trying to remove buffer thing

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -379,7 +379,6 @@ export default class AgendaView extends Component {
             { useNativeDriver: true },
           )}
         >
-          <View style={{height: agendaHeight + KNOB_HEIGHT}} onLayout={this.onScrollPadLayout} />
         </Animated.ScrollView>
       </View>
     );
